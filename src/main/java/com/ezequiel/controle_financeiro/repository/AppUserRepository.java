912aa225cec7b94268397b2bbe835ddde1deabb4
Ezequiel <kiel02505@gmail.com>
2025-08-24 17:47:05 -0300

    feat: add AppUserRepository

---
+++ src/main/java/com/ezequiel/controle_financeiro/repository/AppUserRepository.java
@@ -0,0 +1,11 @@
+package com.ezequiel.controle_financeiro.repository;
+
+import com.ezequiel.controle_financeiro.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, UUID> {
+}

a5e49dfc536fc32b246c2181b7182e6f04441509
Ezequiel <kiel02505@gmail.com>
2025-09-07 20:58:50 -0300

    feat: add findByEmail and createUser methods

--- src/main/java/com/ezequiel/controle_financeiro/service/AppUserService.java
+++ src/main/java/com/ezequiel/controle_financeiro/service/AppUserService.java
@@ -1,11 +1,14 @@
 package com.ezequiel.controle_financeiro.service;
 
+import com.ezequiel.controle_financeiro.dto.AppUserRequestDTO;
 import com.ezequiel.controle_financeiro.dto.AppUserResponseDTO;
 import com.ezequiel.controle_financeiro.model.AppUser;
 import com.ezequiel.controle_financeiro.repository.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -19,4 +22,22 @@ public class AppUserService {
         return new AppUserResponseDTO(user.getId(), user.getName(), user.getEmail(), user.getUserRole(), user.getCreatedAt());
     }
 
+    public Optional<AppUser> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public AppUserResponseDTO createUser(AppUserRequestDTO dto) {
+        AppUser appUser = new AppUser();
+        appUser.setName(dto.name());
+        appUser.setEmail(dto.email());
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(dto.password());
+
+        appUser.setPasswordHash(encryptedPassword);
+        appUser.setUserRole(dto.userRole());
+
+        AppUser newAppUser = this.userRepository.save(appUser);
+
+        return new AppUserResponseDTO(newAppUser.getId(), newAppUser.getName(), newAppUser.getEmail(), newAppUser.getUserRole(), newAppUser.getCreatedAt());
+    }
 }

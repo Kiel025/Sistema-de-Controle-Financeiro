4dd5a2f4b4c43ea9249e539d4f06fcc0fbb2c199
Ezequiel <kiel02505@gmail.com>
2025-09-07 20:57:32 -0300

    feat: add LoginResponse and LoginRequest DTOs

---
+++ src/main/java/com/ezequiel/controle_financeiro/security/model/dto/LoginRequestDTO.java
@@ -0,0 +1,4 @@
+package com.ezequiel.controle_financeiro.security.model.dto;
+
+public record LoginRequestDTO(String email, String password) {
+}
---
+++ src/main/java/com/ezequiel/controle_financeiro/security/model/dto/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package com.ezequiel.controle_financeiro.security.model.dto;
+
+public record LoginResponseDTO(String accessToken) {
+}
